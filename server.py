d7449602b3b30a0182e56f48e11e092d6a319ba2
Hanbings <3219065882@qq.com>
2021-02-17 09:33:15 +0800

    :fire: Feat: Changed to Flask Again

--- server.py
+++ server.py
@@ -1,11 +1,29 @@
-import asyncio
-import websockets
+from flask import Flask,send_file,request
+from datetime import timedelta
+import requests
 
-async def upload(websocket, path):
-      async for message in websocket:
-        message = "I got your message: {}".format(message)
-        await websocket.send(message)
+app = Flask(__name__,static_url_path='')
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = timedelta(seconds=1)
+ 
+@app.route('/')
+def main():
+    return app.send_static_file("index.html")
+ 
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    url = "http://member.bilibili.com/x/vu/web/cover/up"
 
-print("[BDT] Server Running")
-asyncio.get_event_loop().run_until_complete(websockets.serve(upload, 'localhost', 10086))
-asyncio.get_event_loop().run_forever()
+    payload = "cover="+str(request.json.get('base64'))+"&csrf="+str(request.json.get('bilijct'))
+    headers = {
+    'Content-Type': "application/x-www-form-urlencoded",
+    'Cookie': "SESSDATA="+str(request.json.get('sessdata'))+"; bili_jct="+str(request.json.get('bilijct'))+";",
+    'cache-control': "no-cache"
+    }
+
+    response = requests.request("POST", url, data=payload, headers=headers,verify=False)
+    print(payload)
+    print(response.text)
+    return response.text
+
+if __name__ == '__main__':
+    app.run()
---
+++ static/js/main.js
@@ -0,0 +1,53 @@
+/**正如你所见 我刻意隐藏了上传到Bilibili的代码
+ * 您放心 我不会保留您的任何数据
+ * 如果您迫切希望得到这一部分代码
+ * 您可以来联系我
+ * Bilibili 不寂寒冰
+ */
+var base64;
+var bvid;
+var sessdata;
+var bilijct;
+var server = '/upload'
+
+function saveBvid(obj) {
+    bvid = obj.value;
+}
+
+function saveSessdata(obj) {
+    sessdata = obj.value;
+}
+
+function saveBilibiliJct(obj) {
+    bilijct = obj.value;
+}
+
+function imgChange(obj) {
+    var image = obj.files[0]; //获取文件域中选中的图片
+    var reader = new FileReader(); //实例化文件读取对象
+    reader.readAsDataURL(image); //将文件读取为 DataURL,也就是base64编码
+    reader.onload = function(ev) { //文件读取成功完成时触发
+        var dataURL = ev.target.result; //获得文件读取成功后的DataURL,也就是base64编码
+        console.log(dataURL);
+        base64 = dataURL;
+    }
+}
+
+function upload() {
+    // 上传
+    var httpRequest = new XMLHttpRequest(); //第一步：创建需要的对象
+    httpRequest.open('POST', server, true); //第二步：打开连接/***发送json格式文件必须设置请求头 ；如下 - */
+    httpRequest.setRequestHeader("Content-type", "application/json"); //设置请求头 注：post方式必须设置请求头（在建立连接后设置请求头）
+    var obj = { 'bvid': bvid, 'sessdata': sessdata, 'bilijct': bilijct, 'base64': base64 };
+    httpRequest.send(JSON.stringify(obj)); //发送请求 将json写入send中
+    /**
+     * 获取数据后的处理程序
+     */
+    httpRequest.onreadystatechange = function() { //请求后的回调接口，可将请求成功后要执行的程序写在其中
+        if (httpRequest.readyState == 4 && httpRequest.status == 200) { //验证请求是否发送成功
+            var json = httpRequest.responseText; //获取到服务端返回的数据
+            alert(json);
+        }
+    };
+    alert('bwt 我不会保留你们任何的数据 以及......能不能过审看运气咯～')
+}
